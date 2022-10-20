7ae48be4e1f63cbf60ca4d62f2737ef1ba6fdc1c
Indah3112 <indahiw1231@gmail.com>
2022-10-20 12:08:22 +0700

    coding day 1

---
+++ operator aritmatika.py
@@ -0,0 +1,38 @@
+#file /python_dasar/operator_aritmatika.py
+#OPERATOR ARITMATIKA
+#Penjumlahan
+print (13 + 2)
+Apel = 7
+Jeruk = 9
+Buah = Apel + Jeruk#
+print (Buah)
+#pengurangan
+Hutang = 10000
+Bayar = 5000
+sisaHutang = Hutang - Bayar
+print ("sisa hutang Anda adalah", sisaHutang)
+#perkalian
+panjang = 15
+lebar = 8
+luas = panjang * lebar
+print (luas)
+
+#pembagian
+kue = 16
+anak = 4
+kuePerAnak = kue / anak
+print ("setiap anak mendapatkan bagian kue sebanyak", kuePerAnak)
+#sisa Bagi / Modulus
+bilangan1 = 14
+bilangan2 = 5
+hasil = bilangan1 % bilangan2
+print ("sisa bagi dari bilangan", bilangan1, "dan", bilangan2, "adalah", hasil)
+#pangkat
+bilangan3 = 8
+bilangan4 = 2
+hasilPangkat = bilangan3 ** bilangan4
+print(hasilPangkat)
+#pembagian Bulat
+print (10//3)
+#10 dibagi 3 adalah 3.3333. Karena dibulatkan maka akan menghasilkan nilai 3
+
