58f37af10856f23853dfbc804d673693306ccc84
Indah3112 <indahiw1231@gmail.com>
2022-10-20 12:05:56 +0700

    coding day 4

---
+++ menginput data dan user.py
@@ -0,0 +1,7 @@
+nama = input ("Masukkan nama:")
+
+print("nama = ",nama,",type =",type(nama))
+
+usia =int(input("masukkan usia: "))
+
+print ("data = ",usia,",type =",type(usia))
--- tipe data casting.py
+++ tipe data casting.py
@@ -1,5 +1,5 @@
-data_int = 9;
-print("data = ", data_int, ",type =",type(data_int))
+data_int = ;
+print("data = ", data_int, ",type =",type(data_  int))
 
 data_float = float(data_int)
 data_str = str(data_int)
