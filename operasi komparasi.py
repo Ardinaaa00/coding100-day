c6d46c5b3ae3a730ab6fefab2ab469b39b7236a6
Indah3112 <indahiw1231@gmail.com>
2022-10-21 16:03:58 +0700

    coding day lima

---
+++ operasi komparasi.py
@@ -0,0 +1,47 @@
+a = 3
+b = 4
+
+print ('======lebih besar(>)======')
+hasil = a > 1
+print (a, '>',1,'=',hasil)
+
+hasil = b > 6
+print (b, '>',6, '=', hasil)
+
+
+print ('======kurang dari(<)======')
+hasil = a < 1
+print (a, '<',1,'=',hasil)
+
+hasil = b < 6
+print (b, '<',6, '=', hasil)
+
+
+print ('======lebih dari sama dengan(>=)======')
+hasil = a >= 1
+print (a, '>=',1,'=',hasil)
+
+hasil = b >= 6
+print (b, '>=',6, '=', hasil)
+
+print ('======kurang dari sama dengan(<=)======')
+hasil = a <= 1
+print (a, '<=',1,'=',hasil)
+
+hasil = b <= 6
+print (b, '<=',6, '=', hasil)
+
+print ('======sama dengan(==)======')
+hasil = a == 3
+print (a, '==',3,'=',hasil)
+
+hasil = b == 6
+print (b, '==',6, '=', hasil)
+
+print ('======tidak sama dengan(1=)======')
+hasil = a != 4
+print (a, '!=',4,'=',hasil)
+
+hasil = b != 4
+print (b, '!=',4, '=', hasil)
+
