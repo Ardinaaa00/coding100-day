4a31496c5f1a684996c70654e540985d4d1555ff
Indah3112 <indahiw1231@gmail.com>
2022-10-22 13:34:42 +0700

    coding day 6

---
+++ operasi logika.py
@@ -0,0 +1,69 @@
+print ('=====NOT=====')
+a = True
+b = not a
+print('data a =',a)
+print ('-----NOT-----')
+print('data b =',b)
+
+print ('=====OR=====')
+a = False
+b = False
+c = a or b
+print(a, 'OR',b'=',c)
+
+a = False
+b = True
+c = a or b
+print(a, 'OR',b'=',c)
+
+a = True
+b = False
+c = a or b
+print(a, ' OR',b'=',c)
+
+a = True
+b = True
+c = a or b
+print(a, ' OR',b'=',c)
+
+print ('=====AND=====')
+a = False
+b = False
+c = a or b
+print(a, 'AND',b'=',c)
+
+a = False
+b = True
+c = a or b
+print(a, 'AND',b'=',c)
+
+a = True
+b = False
+c = a or b
+print(a, 'AND',b'=',c)
+
+a = True
+b = True
+c = a or b
+print(a, 'AND',b'=',c)
+
+print ('=====XOR=====')
+a = False
+b = False
+c = a or b
+print(a, 'XOR',b'=',c)
+
+a = False
+b = True
+c = a or b
+print(a, 'XOR',b'=',c)
+
+a = True
+b = False
+c = a or b
+print(a, 'XOR',b'=',c)
+
+a = True
+b = True
+c = a or b
+print(a, 'XOR',b'=',c)
